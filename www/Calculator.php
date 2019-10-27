93410f2233b7fea47a63627206e9620e5b5a8da1
sam_arbid <samarbid13@users.noreply.github.com>
2019-10-27 21:20:51 +0100

    second commit

--- www/Calculator.php
+++ www/Calculator.php
@@ -15,6 +15,27 @@
     <input type="submit" />
 
     </form>
+    <?php
+    $num1 = $_POST["num1"];
+    $num2 = $_POST["num2"];
+    $op = $_POST["op"];
+
     
+    function calc($num1, $num2, $op){
+        if($op == "+"){
+            echo $num1 + $num2;
+        } elseif($op == "/"){
+            echo $num1 / $num2;
+        }elseif($op == "-"){
+            echo $num1 - $num2;
+        }elseif($op == "*"){
+            echo $num1 * $num2;
+        }else {
+            echo "somthing went wrong, check  input";
+        }
+    }
+
+    calc($num1, $num2, $op);
+    ?>
 </body>
 </html>
